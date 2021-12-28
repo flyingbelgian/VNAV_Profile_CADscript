e87b51cd7a68e0e9af3869ad44f26eeec4739174
Paul H <theflyingbelgian@gmail.com>
2021-12-28 13:37:04 +0800

    Added FreeCAD template
    Removed FreeCAD template file from gitignore to be used as reference when calculating points for surface creation.

---
+++ FreeCADslopes.py
@@ -0,0 +1,74 @@
+import FreeCAD, Part, Mesh, MeshPart
+
+DOC = FreeCAD.activeDocument()
+DOC_NAME = "SurfaceExperimenting"
+
+def clear_doc():
+    """Clear the active document deleting all the objects"""
+    for obj in DOC.Objects:
+        DOC.removeObject(obj.Name)
+
+def setview():
+    """Rearrange View"""
+    FreeCAD.Gui.SendMsgToActiveView("ViewFit")
+    FreeCAD.Gui.activeDocument().activeView().viewIsometric()
+    FreeCAD.Gui.activeDocument().activeView().setAxisCross(True)
+
+def makeSurf(V1,V2,V3,V4):
+    """Make a surface bound by 4 vertices"""
+    L1 = Part.LineSegment(V1,V2)
+    L3 = Part.LineSegment(V3,V4)
+    E1 = Part.Edge(L1)
+    E3 = Part.Edge(L3)
+    surf = Part.makeRuledSurface(E1, E3)
+    return surf
+
+def fuseObjects(name,objects):
+    obj = DOC.addObject("Part::MultiFuse", name)
+    obj.Shapes = objects
+    return obj
+
+if DOC is None:
+    FreeCAD.newDocument(DOC_NAME)
+    FreeCAD.setActiveDocument(DOC_NAME)
+    DOC = FreeCAD.activeDocument()
+else:
+    clear_doc()
+
+m = 1
+
+StartWidth = 2000*m
+EndWidth = 4000*m
+Length = 8000*m
+Height = 100*m
+
+THRx = 332638.270*m
+THRy = 6241727.539*m
+THRz = 4.915*m
+THR = FreeCAD.Vector(THRx, THRy, THRz)
+THR_angle = 166+(47/60)+(3/3600)
+
+V1 = FreeCAD.Vector(-StartWidth,0,Height)
+V2 = FreeCAD.Vector(-StartWidth/2,0,0)
+V3 = FreeCAD.Vector(StartWidth/2,0,0)
+V4 = FreeCAD.Vector(StartWidth,0,Height)
+V5 = FreeCAD.Vector(-EndWidth,Length,Height)
+V6 = FreeCAD.Vector(-EndWidth/2,Length,0)
+V7 = FreeCAD.Vector(EndWidth/2,Length,0)
+V8 = FreeCAD.Vector(EndWidth,Length,Height)
+
+primSurf = DOC.addObject("Part::Feature", "Primary")
+primSurf.Shape = makeSurf(V2,V3,V6,V7)
+
+secSurf_pos = DOC.addObject("Part::Feature", "Secondary 1")
+secSurf_pos.Shape = makeSurf(V1,V2,V5,V6)
+
+secSurf_neg = DOC.addObject("Part::Feature", "Secondary 2")
+secSurf_neg.Shape = makeSurf(V3,V4,V7,V8)
+
+Surface = fuseObjects("Surface 1", (primSurf, secSurf_pos, secSurf_neg))
+
+Surface.Placement=FreeCAD.Placement(THR, FreeCAD.Rotation(-THR_angle,0,0), FreeCAD.Vector(0,0,0))
+
+DOC.recompute()
+setview()
