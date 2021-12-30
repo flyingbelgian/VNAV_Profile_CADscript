80d18b8d4c93f5495623117a2c9f1465ee9145d1
Paul H <theflyingbelgian@gmail.com>
2021-12-30 11:43:40 +0800

    Simple standard 3D
    Simple 3D with assumptions that nothing is other than default values.

--- FreeCADslopes.py
+++ FreeCADslopes.py
@@ -3,6 +3,17 @@ import FreeCAD, Part, csv
 DOC = FreeCAD.activeDocument()
 DOC_NAME = "SurfaceExperimenting"
 
+####### At the moment this always assumes Cat D aicraft, functionality for different aircraft categories to be added later
+
+m = 1 #this value set to 1000 or as required to obtain correct final dimensions
+z = 5 #this value is multiplier for elevation to exaggerate vertical dimension
+
+THRx = 332638.270*m
+THRy = 6241727.539*m
+THRz = 4.915*m*z
+THR = FreeCAD.Vector(THRx, THRy, THRz)
+THR_angle = 166+(47/60)+(3/3600)
+
 def clear_doc():
     """Clear the active document deleting all the objects"""
     for obj in DOC.Objects:
@@ -23,15 +34,15 @@ def makeSurf(V1,V2,V3,V4):
     surf = Part.makeRuledSurface(E1, E3)
     return surf
 
-def makeSegment(width_start, width_end, x_start, x_end):
-    V1 = FreeCAD.Vector(x_start,width_start,0)
-    V2 = FreeCAD.Vector(x_start,width_start/2,0)
-    V3 = FreeCAD.Vector(x_start,-width_start/2,0)
-    V4 = FreeCAD.Vector(x_start,-width_start,0)
-    V5 = FreeCAD.Vector(x_end,width_end,0)
-    V6 = FreeCAD.Vector(x_end,width_end/2,0)
-    V7 = FreeCAD.Vector(x_end,-width_end/2,0)
-    V8 = FreeCAD.Vector(x_end,-width_end,0)
+def makeSegment(x_start, y_start, z_start, moc_start, x_end, y_end, z_end, moc_end):
+    V1 = FreeCAD.Vector(x_start *m,  y_start *m, (z_start + moc_start) *m *z)
+    V2 = FreeCAD.Vector(x_start *m,  y_start/2 *m, z_start *m *z)
+    V3 = FreeCAD.Vector(x_start *m, -y_start/2 *m, z_start *m *z)
+    V4 = FreeCAD.Vector(x_start *m, -y_start *m, (z_start + moc_start) *m *z)
+    V5 = FreeCAD.Vector(x_end *m,  y_end *m, (z_end + moc_end) *m *z)
+    V6 = FreeCAD.Vector(x_end *m,  y_end/2 *m, z_end *m *z)
+    V7 = FreeCAD.Vector(x_end *m, -y_end/2 *m, z_end *m *z)
+    V8 = FreeCAD.Vector(x_end *m, -y_end *m, (z_end + moc_end) *m *z)
     primsurf = makeSurf(V2,V3,V6,V7)
     secsurf1 = makeSurf(V1,V2,V5,V6)
     secsurf2 = makeSurf(V3,V4,V7,V8)
@@ -46,31 +57,56 @@ else:
     clear_doc()
 
 #Import parameters
-parameters = {}
+param = {}
 with open("D:\GitHub\VNAV_Profile_CADscript\parameters.csv") as source:
     data = csv.reader(source)
     for row in data:
-        parameters[row[0]] = float(row[1])
+        param[row[0]] = float(row[1])
 
-m = 1 #this value set to 1000 or as required to obtain correct final dimensions
+#Surface start
+zOAS_FAF = (param['xFAF'] - param['xFAS']) * param['grad_FAS']
 
-THRx = 332638.270*m
-THRy = 6241727.539*m
-THRz = 4.915*m
-THR = FreeCAD.Vector(THRx, THRy, THRz)
-THR_angle = 166+(47/60)+(3/3600)
-
-#Surface 1 - Nominal FAF to end of splay in
-segment1 = makeSegment(parameters['SW_FAF']*m, parameters['SW_MAPt']*m, parameters['Dist_FAFtoTHR']*m, parameters['xFAF_splay']*m)
+#Segment 1 - Nominal FAF to end of splay in
+zOAS_FAFsplay = (param['xFAF_splay'] - param['xFAS']) * param['grad_FAS']
+segment1 = makeSegment(
+    param['xFAF'], param['SW_FAF'], zOAS_FAF, param['MOC_final'],
+    param['xFAF_splay'], param['SW_MAPt'], zOAS_FAFsplay, param['MOC_final'])
 Part.show(segment1, "Segment_1")
 
-#Surface2 - end of splay in to start of splay out
-segment2 = makeSegment(parameters['SW_MAPt']*m, parameters['SW_MAPt']*m, parameters['xFAF_splay']*m, parameters['xMAPt_start']*m)
+#Segment2 - end of splay in to xFAS
+zOAS_xFAS = 0
+segment2 = makeSegment(
+    param['xFAF_splay'], param['SW_MAPt'], zOAS_FAFsplay, param['MOC_final'],
+    param['xFAS'], param['SW_MAPt'], zOAS_xFAS, param['MOC_final'])
 Part.show(segment2, "Segment_2")
 
-#Surface3 - start splay out to end of splay out in MA
-segment3 = makeSegment(parameters['SW_MAPt']*m, parameters['SW_MA']*m, parameters['xMAPt_start']*m, parameters['xMAPt_splay']*m)
+#Segment3 - xFAS to ground surface with final MOC
+xOAS_end = - (param['MOC_final']-param['MOC_MA_init']) / param['grad_FAS'] + param['xFAS']
+print(xOAS_end)   ### for debugging only!!!
+segment3 = makeSegment(
+    param['xFAS'], param['SW_MAPt'], 0, param['MOC_final'],
+    xOAS_end, param['SW_MAPt'], 0, param['MOC_MA_init'])
 Part.show(segment3, "Segment_3")
+
+#Segment4 - ground surface with final MOC to start missed approach surface
+segment4 = makeSegment(
+    xOAS_end, param['SW_MAPt'], 0, param['MOC_MA_init'],
+    param['xMAPt_start'], param['SW_MAPt'], 0, param['MOC_MA_init'])
+Part.show(segment4, "Segment_4")
+
+#Segment5 - ground surface start of missed approach to xZ
+width_xZ = ((param['xMAPt_start'] - param['xZ_D_calc']) * param['splay_out']) + param['SW_MAPt']
+segment5 = makeSegment(
+    param['xMAPt_start'], param['SW_MAPt'], 0, param['MOC_MA_init'],
+    param['xZ_D_calc'], width_xZ, 0, param['MOC_MA_init'])
+Part.show(segment5, "Segment_5")
+
+#Segment6 - xZ to end of MA splay
+zOAS_MAsplay = - (param['xMAPt_splay'] - param['xZ_D_calc']) * param['grad_MA']
+segment6 = makeSegment(
+    param['xZ_D_calc'], width_xZ, 0, param['MOC_MA_init'],
+    param['xMAPt_splay'], param['SW_MA'], zOAS_MAsplay, param['MOC_MA_init'])
+Part.show(segment6, "Segment_6")
 
 #Surface.Placement=FreeCAD.Placement(THR, FreeCAD.Rotation(-THR_angle,0,0), FreeCAD.Vector(0,0,0))
 
--- calculator.py
+++ calculator.py
@@ -23,32 +23,32 @@ def getSource():
 def addCalcVariables(values):
     '''Outputs a dictionary with calculated reference values to add to the dictionary of parameters'''
     new_values = {}
-    Dist_FAPtoTHR = (values['Alt_FAP'] - values['Elev_THR'] - values['RDH']) / values['VPA_Prom']
-    new_values['Dist_FAPtoTHR'] = Dist_FAPtoTHR
-    TAS_Conversion = 171233 * math.pow(((288+values['Temp_DevISA'])-(0.006496*values['Elev_AD'])),0.5) / math.pow(288-(0.006496*values['Elev_AD']),2.628)
-    new_values['TAS_Conversion'] = TAS_Conversion
-    new_values['TAS_Final_A'] = values['IAS_Final_A'] * TAS_Conversion
-    new_values['TAS_Final_B'] = values['IAS_Final_B'] * TAS_Conversion
-    new_values['TAS_Final_C'] = values['IAS_Final_C'] * TAS_Conversion
-    new_values['TAS_Final_D'] = values['IAS_Final_D'] * TAS_Conversion
-    new_values['OCH_A'] = values['OCA_A'] - values['Elev_THR']
-    new_values['OCH_B'] = values['OCA_B'] - values['Elev_THR']
-    new_values['OCH_C'] = values['OCA_C'] - values['Elev_THR']
-    new_values['OCH_D'] = values['OCA_D'] - values['Elev_THR']
+    xFAP = (values['zFAP'] - values['zTHR'] - values['RDH']) / values['VPA_prom']
+    new_values['xFAP'] = xFAP
+    TAS_conv = 171233 * math.pow(((288+values['temp_devISA'])-(0.006496*values['zAD'])),0.5) / math.pow(288-(0.006496*values['zAD']),2.628)
+    new_values['TAS_conversion'] = TAS_conv
+    new_values['TAS_final_A'] = values['IAS_final_A'] * TAS_conv
+    new_values['TAS_final_B'] = values['IAS_final_B'] * TAS_conv
+    new_values['TAS_final_C'] = values['IAS_final_C'] * TAS_conv
+    new_values['TAS_final_D'] = values['IAS_final_D'] * TAS_conv
+    new_values['OCH_A'] = values['OCA_A'] - values['zTHR']
+    new_values['OCH_B'] = values['OCA_B'] - values['zTHR']
+    new_values['OCH_C'] = values['OCA_C'] - values['zTHR']
+    new_values['OCH_D'] = values['OCA_D'] - values['zTHR']
     return new_values
 
 def addHL(values):
     '''Outputs a dictionary with calculated HL allowances to add to the dictionary of parameters'''
     new_values = {}
-    HLadjelev = 0
-    if values['Elev_AD'] > 900:
-        HLadjelev = 2 * values['Elev_AD'] / 300 / 100
-    HLadjGP = 0
-    if values['VPA_Prom'] > 3.5:
+    HLadj_elev = 0
+    if values['zAD'] > 900:
+        HLadj_elev = 2 * values['zAD'] / 300 / 100
+    HLadj_GP = 0
+    if values['VPA_prom'] > 3.5:
         print("Non-Standard VPA")
-    elif values['VPA_Prom'] > 3.2:
-        HLadjGP = 5 * (values['VPA_Prom'] - 3.2) * 10 / 100
-    HLadj = (1+HLadjelev) * (1+HLadjGP)
+    elif values['VPA_prom'] > 3.2:
+        HLadj_GP = 5 * (values['VPA_prom'] - 3.2) * 10 / 100
+    HLadj = (1+HLadj_elev) * (1+HLadj_GP)
     new_values['HL_A_calc'] = values['HL_A_std'] * HLadj
     new_values['HL_B_calc'] = values['HL_B_std'] * HLadj
     new_values['HL_C_calc'] = values['HL_C_std'] * HLadj
@@ -58,48 +58,48 @@ def addHL(values):
 def addTempCorr(values):
     '''Outputs a dictionary with calculated temperature correction to add to the dictionary of parameters'''
     new_values = {}
-    deltatemp = values['Temp_DevISA']
+    deltatemp = values['temp_devISA']
     Lo = values['Lo']
-    hFAP = values['Alt_FAP'] - values['Elev_THR']
+    hFAP = values['zFAP'] - values['zTHR']
     To = values['To']
-    hTHR = values['Elev_THR']
-    TempCorr = (-deltatemp/Lo) * math.log(1 + Lo * hFAP / (To + Lo * hTHR))
-    new_values['TempCorr'] = TempCorr
+    hTHR = values['zTHR']
+    temp_corr = (-deltatemp/Lo) * math.log(1 + Lo * hFAP / (To + Lo * hTHR))
+    new_values['temp_corr'] = temp_corr
     return new_values
 
 def addOAS(values):
     '''Outputs a dictionary with OAS parameters to add to the dictionary of parameters'''
     new_values = {}
-    hFAP = values['Alt_FAP'] - values['Elev_THR']
+    hFAP = values['zFAP'] - values['zTHR']
     ypsilon = 0.08 * values['g']
     Hi = values['H0']
-    if values['Elev_THR'] > (10000 * values['ft2m']):
+    if values['zTHR'] > (10000 * values['ft2m']):
         Hi = values['H10000']
-    elif values['Elev_THR'] > (5000 * values['ft2m']):
+    elif values['zTHR'] > (5000 * values['ft2m']):
         Hi = values['H5000']
-    VPA_Min = (hFAP - values['TempCorr'] - values['RDH']) / values['Dist_FAPtoTHR']
-    new_values['VPA_Min'] = VPA_Min
-    grad_FAS = (hFAP + values['TempCorr'] - Hi) * (values['VPA_Prom'] / (hFAP - Hi))
+    VPA_min = (hFAP - values['temp_corr'] - values['RDH']) / values['xFAP']
+    new_values['VPA_min'] = VPA_min
+    grad_FAS = (hFAP + values['temp_corr'] - Hi) * (values['VPA_prom'] / (hFAP - Hi))
     new_values['grad_FAS'] = grad_FAS
-    xFAS = ((Hi - values['RDH']) / values['VPA_Prom']) + values['ATT_FAF']
+    xFAS = ((Hi - values['RDH']) / values['VPA_prom']) + values['ATT_FAF']
     new_values['xFAS'] = xFAS
     xSEC30 = xFAS - ((Hi - 30) / grad_FAS) #point at which secondary surface height reaches 30m above ground plane
     new_values['xSEC30'] = xSEC30
-    altFAF = (values['Dist_FAFtoTHR'] - xFAS) * grad_FAS
-    new_values['altFAF'] = altFAF    
-    xZ_A_adj = (values['HL_A_calc'] - values['RDH']) / values['VPA_Prom'] \
-                 - (values['ATT_MAPt'] + 2*values['TAS_Final_A']*math.sin(math.atan(values['VPA_Prom'])) \
-                 /ypsilon*(values['TAS_Final_A']+values['Wind_MA']))
-    xZ_B_adj = (values['HL_B_calc'] - values['RDH']) / values['VPA_Prom'] \
-                 - (values['ATT_MAPt'] + 2*values['TAS_Final_B']*math.sin(math.atan(values['VPA_Prom'])) \
-                 /ypsilon*(values['TAS_Final_B']+values['Wind_MA']))
-    xZ_C_adj = (values['HL_C_calc'] - values['RDH']) / values['VPA_Prom'] \
-                 - (values['ATT_MAPt'] + 2*values['TAS_Final_C']*math.sin(math.atan(values['VPA_Prom'])) \
-                 /ypsilon*(values['TAS_Final_C']+values['Wind_MA']))
-    xZ_D_adj = (values['HL_D_calc'] - values['RDH']) / values['VPA_Prom'] \
-                 - (values['ATT_MAPt'] + 2*values['TAS_Final_D']*math.sin(math.atan(values['VPA_Prom'])) \
-                 /ypsilon*(values['TAS_Final_D']+values['Wind_MA']))
-    if values['Elev_AD'] > 900 or math.degrees(math.atan(values['VPA_Prom'])) > 3.2:
+    altFAF = (values['xFAF'] - xFAS) * grad_FAS
+    new_values['zFAF'] = altFAF    
+    xZ_A_adj = (values['HL_A_calc'] - values['RDH']) / values['VPA_prom'] \
+                 - (values['ATT_MAPt'] + 2*values['TAS_final_A']*math.sin(math.atan(values['VPA_prom'])) \
+                 /ypsilon*(values['TAS_final_A']+values['wind_MA']))
+    xZ_B_adj = (values['HL_B_calc'] - values['RDH']) / values['VPA_prom'] \
+                 - (values['ATT_MAPt'] + 2*values['TAS_final_B']*math.sin(math.atan(values['VPA_prom'])) \
+                 /ypsilon*(values['TAS_final_B']+values['wind_MA']))
+    xZ_C_adj = (values['HL_C_calc'] - values['RDH']) / values['VPA_prom'] \
+                 - (values['ATT_MAPt'] + 2*values['TAS_final_C']*math.sin(math.atan(values['VPA_prom'])) \
+                 /ypsilon*(values['TAS_final_C']+values['wind_MA']))
+    xZ_D_adj = (values['HL_D_calc'] - values['RDH']) / values['VPA_prom'] \
+                 - (values['ATT_MAPt'] + 2*values['TAS_final_D']*math.sin(math.atan(values['VPA_prom'])) \
+                 /ypsilon*(values['TAS_final_D']+values['wind_MA']))
+    if values['zAD'] > 900 or math.degrees(math.atan(values['VPA_prom'])) > 3.2:
         new_values['xZ_A_calc'] = min(values['xZ_A_std'], xZ_A_adj)
         new_values['xZ_B_calc'] = min(values['xZ_B_std'], xZ_B_adj)
         new_values['xZ_C_calc'] = min(values['xZ_C_std'], xZ_C_adj)
@@ -109,32 +109,32 @@ def addOAS(values):
         new_values['xZ_B_calc'] = values['xZ_B_std']
         new_values['xZ_C_calc'] = values['xZ_C_std']
         new_values['xZ_D_calc'] = values['xZ_D_std']
-    new_values['xSOC_A'] = new_values['xZ_A_calc'] + ((values['OCH_A'] - values['HL_A_calc']) / values['VPA_Prom'])
-    new_values['xSOC_B'] = new_values['xZ_B_calc'] + ((values['OCH_B'] - values['HL_B_calc']) / values['VPA_Prom'])
-    new_values['xSOC_C'] = new_values['xZ_C_calc'] + ((values['OCH_C'] - values['HL_C_calc']) / values['VPA_Prom'])
-    new_values['xSOC_D'] = new_values['xZ_D_calc'] + ((values['OCH_D'] - values['HL_D_calc']) / values['VPA_Prom'])
+    new_values['xSOC_A'] = new_values['xZ_A_calc'] + ((values['OCH_A'] - values['HL_A_calc']) / values['VPA_prom'])
+    new_values['xSOC_B'] = new_values['xZ_B_calc'] + ((values['OCH_B'] - values['HL_B_calc']) / values['VPA_prom'])
+    new_values['xSOC_C'] = new_values['xZ_C_calc'] + ((values['OCH_C'] - values['HL_C_calc']) / values['VPA_prom'])
+    new_values['xSOC_D'] = new_values['xZ_D_calc'] + ((values['OCH_D'] - values['HL_D_calc']) / values['VPA_prom'])
     return new_values
 
 def addLateralLimits(values):
     '''Outputs a dictionary with calculated lateral limits for the start, finish and splays of the protection areas'''
     new_values = {}
-    xFAF_start = values['Dist_FAFtoTHR'] + values['ATT_FAF']
+    xFAF_start = values['xFAF'] + values['ATT_FAF']
     new_values['xFAF_start'] = xFAF_start
-    xFAF_splay = values['Dist_FAFtoTHR'] - (values['SW_FAF'] - values['SW_MAPt'])/values['Splay_in']
+    xFAF_splay = values['xFAF'] - (values['SW_FAF'] - values['SW_MAPt'])/values['splay_in']
     new_values['xFAF_splay'] = xFAF_splay
-    xMAPt_start = values['Dist_MAPTtoTHR'] + values['ATT_MAPt']
+    xMAPt_start = values['xMAPt'] + values['ATT_MAPt']
     new_values['xMAPt_start'] = xMAPt_start
-    xMAPt_splay = xMAPt_start - (values['SW_MA'] - values['SW_MAPt'])/values['Splay_out']
+    xMAPt_splay = xMAPt_start - (values['SW_MA'] - values['SW_MAPt'])/values['splay_out']
     new_values['xMAPt_splay'] = xMAPt_splay
-    xVNAV_end_A = -values['Dist_THRtoMATP']
-    xVNAV_end_B = -values['Dist_THRtoMATP']
-    xVNAV_end_C = -values['Dist_THRtoMATP']
-    xVNAV_end_D = -values['Dist_THRtoMATP']
-    if values['Dist_THRtoMATP'] == 0:
-        xVNAV_end_A = -((values['Alt_MAturn'] - values['OCA_A'] + values['HL_A_calc']) / values['Grad_MA']) + values['xSOC_A']
-        xVNAV_end_B = -((values['Alt_MAturn'] - values['OCA_B'] + values['HL_B_calc']) / values['Grad_MA']) + values['xSOC_B']
-        xVNAV_end_C = -((values['Alt_MAturn'] - values['OCA_C'] + values['HL_C_calc']) / values['Grad_MA']) + values['xSOC_C']
-        xVNAV_end_D = -((values['Alt_MAturn'] - values['OCA_D'] + values['HL_D_calc']) / values['Grad_MA']) + values['xSOC_D']
+    xVNAV_end_A = -values['xMATP']
+    xVNAV_end_B = -values['xMATP']
+    xVNAV_end_C = -values['xMATP']
+    xVNAV_end_D = -values['xMATP']
+    if values['xMATP'] == 0:
+        xVNAV_end_A = -((values['zMATP'] - values['OCA_A'] + values['HL_A_calc']) / values['grad_MA']) + values['xSOC_A']
+        xVNAV_end_B = -((values['zMATP'] - values['OCA_B'] + values['HL_B_calc']) / values['grad_MA']) + values['xSOC_B']
+        xVNAV_end_C = -((values['zMATP'] - values['OCA_C'] + values['HL_C_calc']) / values['grad_MA']) + values['xSOC_C']
+        xVNAV_end_D = -((values['zMATP'] - values['OCA_D'] + values['HL_D_calc']) / values['grad_MA']) + values['xSOC_D']
     new_values['xVNAV_end_A'] = xVNAV_end_A
     new_values['xVNAV_end_B'] = xVNAV_end_B
     new_values['xVNAV_end_C'] = xVNAV_end_C
